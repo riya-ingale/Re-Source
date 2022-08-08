c6176527f835216549d6fec498d7bd4ec8aa54bf
riya-ingale <riya.ingale14@gmail.com>
2022-08-08 18:45:31 +0530

    get details started

--- ResourceApp/urls.py
+++ ResourceApp/urls.py
@@ -4,6 +4,7 @@ from ResourceApp import views
 urlpatterns = [
     path('',views.getresources,name= "getResources"),
     path('add/<username>/<lab_id>',views.addresources, name = 'addResources'),
+    path('getdetails/<r_id>',views.getdetails,name = "getDetails")
     # path('add/<lab_id>',views.addresources, name = 'addResources')
     # path('getsearchedresources',views.getsearchedresources,name='getSearchedResources')
 ]
