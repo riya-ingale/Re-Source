be60505caa1c8fcb2b8346b8bafd25ee65256b59
Rajeev064 <rs3167196@gmail.com>
2022-08-08 09:54:38 +0530

    Integration Address

--- ResourceApp/urls.py
+++ ResourceApp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 # <<<<<<< HEAD
     path('add/<username>',views.addresources, name = 'addResources'),
 # =======
-    path('add/<lab_id>',views.addresources, name = 'addResources')
+    # path('add/<lab_id>',views.addresources, name = 'addResources')
     # path('getsearchedresources',views.getsearchedresources,name='getSearchedResources')
 # >>>>>>> 8495d0bea87d6462f0860acfbdc48e38fc290c28
 ]
