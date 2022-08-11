62300806d5a93f741500a8545f384ad00a626a5f
Rajeev064 <rs3167196@gmail.com>
2022-08-11 21:03:03 +0530

    integrated view details - without onload, book slot and image

--- ResourceApp/urls.py
+++ ResourceApp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('',views.getresources,name= "getResources"),
     path('add/<username>/<lab_id>',views.addresources, name = 'addResources'),
     path('getdetails/<r_id>',views.getdetails,name = "getDetails"),
-    path('edit_resource/<id>' , views.resource_edit)
+    # path('edit_resource/<id>' , views.resource_edit)
     # path('addslots/', views.addslots)
     # path('add/<lab_id>',views.addresources, name = 'addResources')
     # path('getsearchedresources',views.getsearchedresources,name='getSearchedResources')
