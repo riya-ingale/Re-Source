e247e7aaf9b0f27af02a17eb34222e3644d4e7ad
Rajeev064 <rs3167196@gmail.com>
2022-08-17 21:53:49 +0530

    changes in payment

--- ReSource-FE/src/Components/Paynow.js
+++ ReSource-FE/src/Components/Paynow.js
@@ -8,23 +8,33 @@ export default function App() {
     // const order = await createOrder(params);
     const options = {
       "key": "rzp_test_DfplOiJGao9t7P",
-      "amount": "200",
+      "id": "order_EKwxwAgItmmXdp",
+      "entity": "order",
+      "amount": 50000,
+      "amount_paid": 0,
+      "amount_due": 50000,
       "currency": "INR",
-      "name": "Re-Source Resources",
-      "description": "Test Transaction",
-      "order_id": "order_K6irE9WSJWtQBU",
-      "callback_url": "http://127.0.0.1:8000/placeorder/handlerequest/",
-      "prefill": {
-          "name": "ABS",
-          "email": "abs@gmail.com",
-          "contact": "+919876543212"
+      "receipt": "receipt#1",
+      "offer_id": null,
+      "status": "created",
+      "attempts": 0,
+      "notes": [],
+      "created_at": 1582628071,
+    
+      callback_url: "http://127.0.0.1:8000/placeorder/handlerequest/",
+      prefill: {
+          name: "ABS",
+          email: "abs@gmail.com",
+          contact: "+919876543212"
       },
-      "theme": {
-          "color": "#2BA977"
+      theme: {
+          color: "#2BA977"
       }
   };
 
     const rzpay = new Razorpay(options);
+    console.log(options);
+    console.log(rzpay)
     rzpay.open();
   }, [Razorpay]);
 
