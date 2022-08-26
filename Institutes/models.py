816a4bf83fd46e407f88ffc19462f2aea802116f
gaga1313 <gauravgaonkar13.13@gmail.com>
2022-08-26 13:21:03 +0530

    'userdbadded'

--- Institutes/models.py
+++ Institutes/models.py
@@ -1,6 +1,13 @@
 from tkinter import CASCADE
 from django.db import models
 
+class Usser(models.Model):
+    id = models.AutoField(primary_key = True)
+    email = models.CharField(max_length = 500)
+    password = models.CharField(max_length = 500)
+    role_id = models.IntegerField()
+    institute = models.CharField(max_length=500)
+    
 class Institutes(models.Model):
     id = models.AutoField(primary_key = True)
     registeration_no = models.CharField(max_length = 500 , null = True)
